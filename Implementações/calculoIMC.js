dbcecf0ccdbf4a3b81d45de2d4daa2555a96b5cc
0xDanilo <danilo@riseup.net>
2020-05-14 21:43:06 -0300

    Organizando o código.

--- Implementações/calculoIMC.js
+++ Implementações/calculoIMC.js
@@ -2,17 +2,19 @@
     Implementaçãoo do Algoritimo Calculo de IMC
 */
 
-//Cria as variaveis "peso" e "altura" e solicita a entrada dos valores pelo usuário.
-let peso = Number(prompt("Qual o seu peso?"));
-let altura = Number(prompt("Qual a sua altura?"));
-//Função que calcula o IMC
+ //Função que calcula o IMC
  function calculoIMC(peso, altura) {
      resultado = peso/(Math.pow(altura, 2));
      return resultado;
  }
 
- resultadoIMC = calculoIMC(peso, altura);
- console.log(resultadoIMC); //Mostra o resultado do calculo no console.
+ //Cria as variaveis "peso" e "altura" e solicita a entrada dos valores pelo usuário.
+let peso = Number(prompt("Qual o seu peso?"));
+let altura = Number(prompt("Qual a sua altura?"));
+
+resultadoIMC = calculoIMC(peso, altura); //Chama a função e realiza o cálculo
+console.log(resultadoIMC); //Mostra o resultado do calculo no console.
+
 //Compara o resultado do calculo e retorna a mensagem de acordo com a condição do IMC.
 if (resultadoIMC < 20) {
     alert("Abaixo do peso ideal");
@@ -25,5 +27,4 @@ if (resultadoIMC < 20) {
 }else {
     alert("Obesidade morbida.");
 }
-
 
