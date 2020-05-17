6444387218085ecadabe60dd05e22935a19ec044
0xDanilo <danilo@riseup.net>
2020-05-17 06:14:03 -0300

    Exercicios.

---
+++ Implementações/acumulador.js
@@ -0,0 +1,5 @@
+let soma =  0;
+for (let i = 1; i <= 10; i++) {
+     soma = soma + i;
+}
+console.log(soma);
---
+++ Implementações/tabuadaDo3.js
@@ -0,0 +1,5 @@
+let tabuada = 0;
+for (let i = 0; i <- 10; i++) {
+    tabuada = i * 3;
+    console.log("Resultado: 3 x "+i+" = "+tabuada);
+}
