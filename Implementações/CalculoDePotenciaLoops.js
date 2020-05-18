e9b85a1ecad2403150c385907da69077be0c884a
0xDanilo <danilo@riseup.net>
2020-05-18 18:52:14 -0300

    Repetição: Enquanto e Para.

---
+++ Implementações/CalculoDePotenciaLoops.js
@@ -0,0 +1,25 @@
+/*
+    Repetição: Enquanto e Para
+*/
+
+//Declara as variáveis.
+let resultado = 1;
+let resultado2 = 1;
+let contador = 0;
+
+//Realiza o calculo utilizando um loop 'Enquanto'.
+while (contador < 8) {
+  resultado = resultado * 2;
+  contador = contador + 1; //Controla o laço de repetição, ao atigir 8 para.
+}
+console.log(resultado);
+
+//Realiza o cálculo utilizando um loop 'Para'.
+for (let i = 0; i < 8; i++) {
+  resultado2 = resultado2 * 2;
+}
+console.log(resultado2);
+
+//Realiza o mesmo cálculo utilizando a função nativa em JavaScript.
+console.log(Math.pow(2, 8));
+
