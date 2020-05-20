5e3cc5b8634277df3337b2d7145c252db988b5d0
0xDanilo <danilo@riseup.net>
2020-05-19 22:15:07 -0300

    Correções.

--- Implementações/C/acumulador.c
+++ Implementações/C/acumulador.c
@@ -5,9 +5,9 @@ int main()
     int soma, i;
     
     soma = 0;
-    for ( i = 0; i <= 10; i++) {
+    for ( i = 1; i <= 10; i++) {
         soma = soma + i;
     }
-    printf("O soma é: %i.", soma);
+    printf("O soma de todos os números entre 1 e 10 é: %i.", soma);
     return 0;
 }
